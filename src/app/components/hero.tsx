3b742956b056044ea5a3fa89096daf269e076d68
oluwarotimii <oluwarotimiadewumi@gmail.com>
2024-11-27 05:09:11 +0100

    added more to profile

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.6 }}
           >
-            Frontend Developer
+            Software Developer
           </motion.p>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
