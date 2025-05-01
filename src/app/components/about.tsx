091a5559cab2bcda4dbc8506c5373f78256a9fc6
Oluwarotimi Adewumi <60570935+oluwarotimii@users.noreply.github.com>
2025-05-01 18:09:18 +0100

    Update about.tsx

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 
-
-
 'use client'
 
 import { motion } from 'framer-motion'
@@ -10,7 +8,7 @@ import Image from 'next/image'
 export default function About() {
   return (
     <section id="about" className="py-20 bg-gray-800">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex flex-col items-center">
         <motion.h2 
           className="text-2xl md:text-3xl font-bold mb-8 text-center text-amber-400"
           initial={{ opacity: 0, y: 20 }}
@@ -19,14 +17,15 @@ export default function About() {
         >
           About Me
         </motion.h2>
-        <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-col md:flex-row items-center justify-center md:justify-between w-full">
           <motion.div 
-            className="md:w-1/2 mb-8 md:mb-0"
+            className="md:w-1/2 mb-8 md:mb-0 flex justify-center"
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
           >
-{/*             <Image 
+            {/* Uncomment and add image here */}
+            {/* <Image 
               src="/ro.jpeg?height=400&width=400" 
               alt="Oluwarotimi Adewumi" 
               width={400} 
@@ -35,19 +34,19 @@ export default function About() {
             /> */}
           </motion.div>
           <motion.div 
-            className="md:w-1/2 md:pl-8"
+            className="md:w-1/2 md:pl-8 text-center md:text-left"
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
           >
             <p className="text-base md:text-lg mb-4 text-gray-300">
-            Hi! I'm Oluwarotimi Adewumi, a frontend developer with a passion for creating intuitive, responsive, and functional user interfaces. With a strong foundation in HTML, CSS, JavaScript, and React Native, I specialize in building both web and mobile applications using modern frameworks like React, Next.js, and React Native.
+              Hi! I'm Oluwarotimi Adewumi, a frontend developer with a passion for creating intuitive, responsive, and functional user interfaces. With a strong foundation in HTML, CSS, JavaScript, and React Native, I specialize in building both web and mobile applications using modern frameworks like React, Next.js, and React Native.
             </p>
             <p className="text-base md:text-lg mb-4 text-gray-300">
-            y journey in development began with a fascination for how design and technology intersect to create seamless user experiences. This passion has drives me to continuously learn and adapt to the ever-evolving world of frontend and mobile app development.
+              My journey in development began with a fascination for how design and technology intersect to create seamless user experiences. This passion has driven me to continuously learn and adapt to the ever-evolving world of frontend and mobile app development.
             </p>
             <p className="text-base md:text-lg text-gray-300">
-            With over 2 years of experience building intuitive web and mobile applications, I'm always ready and excited to help you bring your next project to life.
+              With over 2 years of experience building intuitive web and mobile applications, I'm always ready and excited to help you bring your next project to life.
             </p>
           </motion.div>
         </div>
